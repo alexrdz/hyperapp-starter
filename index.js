4302227d155c15a22824af587b62df24ac99c61f
Alex Rodriguez <alex@horchatadesign.com>
2018-05-30 21:10:14 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import {h, app} from 'hyperapp';
+
+const state = {
+  count: 0
+};
+
+const actions = {
+  down() {
+    return state => ({count: state.count - 1});
+  },
+
+  up() {
+    return state => ({count: state.count + 1});
+  }
+};
+
+const view = (state, actions) => {
+  return (
+    <main>
+      <h1>{state.count}</h1>
+      <button onclick={actions.down}> - </button>
+      <button onclick={actions.up}> +   </button>
+    </main>
+  );
+};
+
+const main = app(state, actions, view, document.body);
